a1bc9c4c609e1ec420cbb9c2322faa9e9791c1d1
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-10-23 09:24:31 -0400

    updating the demo link

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -114,6 +114,8 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
+    autoplayTimeout: 7000,
+    autoplayHoverPause: true,
     dots: true,
     loop: true,
     items: 1
